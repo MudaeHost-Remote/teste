b17aa441407e5c2d007d531294e6340c3e380900
MudaeHost-Remote <luisfernandonogueirarocha185@gmail.com>
2024-12-28 14:47:14 -0300

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,16 +1,1 @@
-import selfcord
-import os
-
-token = os.getenv("TOKEN")
-channelid = 1322603701222772776
-
-class MyClient(selfcord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
-
-    async def on_message(self, message):
-        if message.channel.id == channelid:
-            await message.channel.send('Hello World')
-
-client = MyClient()
-client.run(token)
+print("Hello World")
